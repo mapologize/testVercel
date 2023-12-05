14a1694a141f9f10d238f62c410f3bca2d7ae3a5
Penzer <dedonoiizunfinity@gmail.com>
2023-12-05 22:35:36 +0700

    new commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const cors = require('cors');
 const home = require("./routes/home");
 
 const app = express();
 
-app.use(cors());
+app.use("/", home);
 app.use(express.json());
 
-app.use("/home", home);
-
-const port = 9001;
+const port = process.env.PORT || 9002;
 
 app.listen(port, () => {
     console.log('App Listening Port : ' + port);
 });
+
+module.exports = app;
